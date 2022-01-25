2550b0c771bb8948da3b6901adfb19c8fcfa2830
deathewillcome3 <45524556+deathewillcome3@users.noreply.github.com>
2022-01-24 20:44:31 -0500

    Delete annoyed.sh

--- annoyed.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kubectl delete -f nextcloud-db.yml
-kubectl delete -f nextcloud-server.yml
-kubectl delete -f kubernetes-storage.yml
-ssh admin@zfs sudo rm -r /storage/containers/db/*
-ssh admin@zfs sudo rm -r /storage/containers/data/*
